fd3d811144995466d04f4ea7b1c24757a50c6f5e
Krikor Herlopian <kherl1@Unh.newhaven.edu>
2023-07-06 23:20:32 -0400

    Working various ways to inject viewModel in compose.

--- androidApp/src/main/java/com/java/cherrypick/android/MainActivity.kt
+++ androidApp/src/main/java/com/java/cherrypick/android/MainActivity.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.java.cherrypick.android.feature.auth.EnterPhoneScreen
-import com.java.cherrypick.feature.auth.presentation.AuthViewModel
-import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = colorResource(id = R.color.cream_white)
                 ) {
-                    val authViewModel: AuthViewModel by inject()
-                    EnterPhoneScreen(authViewModel = authViewModel)
+                    EnterPhoneScreen()
                 }
             }
         }
