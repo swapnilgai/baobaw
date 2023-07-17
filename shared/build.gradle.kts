ab33bc3f2c3d3fd5c74248aadd2d7b12a80b2e28
Swapnil Gaidhankar <swapnilgaidhankar@yahoo.com>
2023-07-17 18:19:42 -0400

    config update

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -18,33 +18,15 @@ kotlin {
         }
     }
 
-//    iosX64 {
-//        binaries {
-//            framework {
-//                baseName = "shared"
-//                export("dev.icerock.moko:resources:${Version.mokoResourcesGenerator}")
-//                export("dev.icerock.moko:graphics:${Version.mokoGraphics}")
-//            }
-//        }
-//    }
-//
-//    iosArm64 {
-//        binaries {
-//            framework {
-//                baseName = "shared"
-//                export("dev.icerock.moko:resources:${Version.mokoResourcesGenerator}")
-//                export("dev.icerock.moko:graphics:${Version.mokoGraphics}")
-//            }
-//        }
-//    }
-
-    ios {
-        binaries {
-            framework {
-                baseName = "shared"
-                export("dev.icerock.moko:resources:${Version.mokoResourcesGenerator}")
-                export("dev.icerock.moko:graphics:${Version.mokoGraphics}")
-            }
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            export("dev.icerock.moko:resources:${Version.mokoResourcesGenerator}")
+            export("dev.icerock.moko:graphics:${Version.mokoGraphics}")
         }
     }
 
@@ -90,14 +72,33 @@ kotlin {
                 }
             }
         }
-        val iosMain by getting {
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+
+        val iosMain by creating {
             dependencies {
                 Dependencies.Shared.iosMain.forEach {
                     implementation(it)
                 }
             }
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
         }
-        val iosTest by getting
+
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
+        }
 
         val appMain by creating {
             dependsOn(commonMain)
