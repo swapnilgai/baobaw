d9cb05881441f4d9c8a4ee0e92ef7dd361d63050
Swapnil Gaidhankar <swapnilgaidhankar@yahoo.com>
2023-07-29 13:58:30 -0400

    retry attempt integration

--- shared/src/commonMain/kotlin/com/java/cherrypick/interactor/Interactor.kt
+++ shared/src/commonMain/kotlin/com/java/cherrypick/interactor/Interactor.kt
@@ -1,7 +1,11 @@
 package com.java.cherrypick.interactor
 
+import io.github.jan.supabase.exceptions.HttpRequestException
+import io.github.jan.supabase.exceptions.RestException
+import io.ktor.utils.io.errors.IOException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
@@ -14,8 +18,8 @@ interface Interactor
 private var retryRequestDeferred: Deferred<Unit>? = null
 suspend fun <T>Interactor.withInteractorContext(
     block: suspend CoroutineScope.() -> T,
-    retryOption: RetryOption = RetryOption(0)
-) : T? {
+    retryOption: RetryOption<T> = RetryOption(0)
+) : T {
 
     val context = InteractorDispatcherProvider.dispatcher
     val interactorErrorHandling = coroutineContext[InteractorErrorHandler]
@@ -24,24 +28,46 @@ suspend fun <T>Interactor.withInteractorContext(
     return withContext(context) {
         var attemptIndex = -1
         var blockResult: T
-
         loop@ while (true) {
             attemptIndex++
             try {
                 retryRequestDeferred?.await()
-                if(attemptIndex>0){
-                    delay(retryOption.delayForRetryAttempt())
-                }
+                if(attemptIndex > 0){ delay(retryOption.delayForRetryAttempt(attemptIndex)) }
+
                 blockResult = coroutineScope {
                     block()
                 }
-
-
+                // check if we should retry based on the given 'retryCondition' and the result of the block
+                if(attemptIndex < retryOption.retryCount
+                    && retryOption.retryCondition(Result.success(blockResult))){
+                    continue
+                }
+               break
             } catch (e: Exception) {
-                throw e.toInteractorException()
+                // check if we should await a retry based on the 'interactorErrorHandling'
+                val awaitRetryOptions = interactorErrorHandling?.awaitRetryOptionOrNull(e)
+                if(awaitRetryOptions != null){
+                    if(retryRequestDeferred == null){ //reuse existing deferred if available
+                        retryRequestDeferred = async { interactorErrorHandling.awaitRetry(awaitRetryOptions) }
+                        retryRequestDeferred?.await()
+                        retryRequestDeferred = null
+                    }
+                    continue
+                }
+
+                // check if we should retry based on the given 'retryCondition' and the exception thrown by the block
+                if(retryOption.retryCondition(Result.failure(e)) && attemptIndex < retryOption.retryCount){
+                    continue
+                }
+                // we have determined we should not attempt to retry: throw an exception
+                throw when {
+                    !isFirstInteractorCall -> e // nested withInteractorContext call: throw the raw exception
+                    e is HttpRequestException || e is RestException -> e.toInteractorException()
+                    else -> e.toInteractorException()
+                }
             }
-            return@withContext blockResult
         }
+        return@withContext blockResult
     }
 }
 
@@ -51,7 +77,7 @@ private data class InteractorCoroutineContextElement(
     companion object Key: CoroutineContext.Key<InteractorCoroutineContextElement>
 }
 
-private fun RetryOption.delayForRetryAttempt(attemptNumber: Int): Long{
+private fun <T> RetryOption<T>.delayForRetryAttempt(attemptNumber: Int): Long{
     return when{
         attemptNumber <= 0 -> 0L
         attemptNumber ==1 -> initialDelay
--- shared/src/commonMain/kotlin/com/java/cherrypick/interactor/RetryOption.kt
+++ shared/src/commonMain/kotlin/com/java/cherrypick/interactor/RetryOption.kt
@@ -1,8 +1,9 @@
 package com.java.cherrypick.interactor
 
-data class RetryOption(
+data class RetryOption<T>(
     val retryCount: Int,
     val initialDelay: Long = 100,
     val maxDelay: Long = 1000,
     val delayIncrementalFactor: Double = 2.0,
+    val retryCondition: (Result<T>) -> Boolean = {it.isFailure}
 )
